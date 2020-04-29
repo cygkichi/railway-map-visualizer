868d5f2e0e3294083b1229cf06932c56f836bbdb
Hiroto Nakano <>
2020-04-29 14:25:28 +0900

    improve get_stationinfo.py

--- get_stationinfo.py
+++ get_stationinfo.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+"""This module is getting station infomtion from wiki."""
 import re
 import urllib
 import logging
@@ -21,6 +23,40 @@ def dms2deg(degree, minute, second):
     """
     deg = degree + minute/60 + second/3600
     return deg
+
+def _get_infoboxtitle(infobox):
+    rows = infobox.find_all('tr',{'class':'','itemprop':'','style':''})
+    infobox_title = rows[0].text
+    return infobox_title
+
+def _get_location(infobox):
+    rows = infobox.find_all('tr',{'class':'','itemprop':'','style':''})
+    latitude = None
+    longitude = None
+    for row in rows:
+        if (not '北緯' in row.text):
+            continue
+        dms_lat  = row.text.split('北緯')[1].split('秒')[0]
+        degree = float(dms_lat.split('度')[0])
+        minute = float(dms_lat.split('度')[1].split('分')[0])
+        second = float(dms_lat.split('度')[1].split('分')[1])
+        latitude = dms2deg(degree,minute,second)
+        dms_lng = row.text.split('東経')[1].split('秒')[0]
+        degree = float(dms_lng.split('度')[0])
+        minute = float(dms_lng.split('度')[1].split('分')[0])
+        second = float(dms_lng.split('度')[1].split('分')[1])
+        longitude = dms2deg(degree,minute,second)
+    return latitude, longitude
+
+def _get_next_urls(infobox):
+    rows = infobox.find_all('tr',{'class':'','itemprop':'','style':''})
+    urls = set()
+    for row in rows:
+        if ('◄' in row.text or '►' in row.text):
+            objs=row.find_all('a',href=re.compile('^(/wiki/)((?!:).)*$'))
+            urls=urls|{urllib.parse.unquote(o.attrs['href']) for o in objs}
+    return urls
+
 
 def get_stationinfo(article_url, wiki_url='https://ja.wikipedia.org'):
     """
@@ -66,42 +102,28 @@ def get_stationinfo(article_url, wiki_url='https://ja.wikipedia.org'):
 
         """Get Station Name"""
         if station_name is None:
-            """First InfoBox."""
-            station_name = rows[0].text
+            station_name = _get_infoboxtitle(infobox)
             logging.debug('Station Name        : '+station_name)            
 
         """Get Location Data"""
         if latitude is None:
-            for row in rows:
-                if ('北緯' in row.text):
-                    dms_lat  = row.text.split('北緯')[1].split('秒')[0]
-                    degree = float(dms_lat.split('度')[0])
-                    minute = float(dms_lat.split('度')[1].split('分')[0])
-                    second = float(dms_lat.split('度')[1].split('分')[1])
-                    latitude = dms2deg(degree,minute,second)
-                    dms_lng = row.text.split('東経')[1].split('秒')[0]
-                    degree = float(dms_lng.split('度')[0])
-                    minute = float(dms_lng.split('度')[1].split('分')[0])
-                    second = float(dms_lng.split('度')[1].split('分')[1])
-                    longitude = dms2deg(degree,minute,second)
-                    logging.debug('Location(lat)       : '+str(latitude))
-                    logging.debug('Location(lng)       : '+str(longitude))                    
-        
+            latitude, longitude = _get_location(infobox)
+            if latitude is not None:
+                logging.debug('Location(lat)       : '+str(latitude))
+                logging.debug('Location(lng)       : '+str(longitude)) 
+
         """Get Next URL  """
-        for row in rows:
-            if ('◄' in row.text or '►' in row.text):
-                objs=row.find_all('a',href=re.compile('^(/wiki/)((?!:).)*$'))
-                urls=[urllib.parse.unquote(o.attrs['href']) for o in objs]
-                for url in urls:
-                    logging.debug('Add Article         : '+url)
-                    next_urls.add(url)
+        urls = _get_next_urls(infobox)
+        next_urls = next_urls | urls
 
         """Get Passengers"""
         for row in rows:
             if('人員' in row.text):
                 passengers_list.append(int(re.sub('\\D','',row.text.split('人/日')[0])))
+
     passengers = max(passengers_list) if passengers_list != [] else 0
     logging.debug('Passengers          : '+str(passengers))
+    logging.debug('Next URLs           : '+str(next_urls))
     logging.debug('##################################')
     logging.debug('End get_stationinfo '+article_url)    
     logging.debug('##################################')
@@ -119,11 +141,11 @@ if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
     article_url_list = ['/wiki/東京駅',\
                         '/wiki/淀屋橋駅',\
-                        '/wiki/扇町駅 (大阪府)']
+                        '/wiki/扇町駅 (大阪府)',\
+                        '/wiki/京橋駅_(大阪府)']
     urls = set()
-    article_url = article_url_list[2]
+    article_url = article_url_list[-1]
     while True:
         urls.add(article_url)
         dic = get_stationinfo(article_url)
         article_url = random.choice(list(dic['next_urls']))
-        print(urls)
